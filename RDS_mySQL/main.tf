d42f499a45218ff61934c82c9ce0002868471fdd
acloud56 <35495677+acloud56@users.noreply.github.com>
2022-05-17 08:09:34 -0400

    Update main.tf

--- RDS_mySQL/main.tf
+++ RDS_mySQL/main.tf
@@ -1,6 +1,3 @@
-# Summary: Create a simple AWS RDS DB Instance with MySQL
-
-# Documentation: https://www.terraform.io/docs/language/settings/index.html
 terraform {
   required_version = ">= 1.0.0"
   required_providers {
@@ -21,9 +18,6 @@ provider "aws" {
   }
 }
 
-# Documentation: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/db_instance
-# Explanation: This resource creates the DB Instance. This Example creates a MySql Server version 5.7 as a Managed RDS Service.
-# Allocates a MySQl version 5.7 in a t3.micro instance with a database name 'changeme_simple_aws_db_instance', username 'changeme_username' and passwd 'changeme_password'
 resource "aws_db_instance" "changeme_simple_aws_db_instance" {
   allocated_storage   = 5
   engine              = "mysql"
@@ -34,14 +28,3 @@ resource "aws_db_instance" "changeme_simple_aws_db_instance" {
   password            = "changeme_password"
   skip_final_snapshot = true
 }
-
-##Explanation:
-# Details on Arguments:
-# Required Argument -  allocated_storage: How Much storage will be allocated for the Database in Gbi
-# Required Argument -             engine: Engines Types like MySql, Postgres, MariaDB....
-# Required Argument -     engine_version: The above choosen version of Engine. For ex:  MySQL version 5.7
-# Required Argument -     instance_class: The size of the Instance (VM)
-# Required Argument -           username: The Master DB-User username
-# Required Argument -            passord: The master DB-User password
-# Optional Argument -               name: The Name of the Instance
-# Optional Argument -  skip_final_snapshot: It's needed for destroying the instance with Terraform Destroy
