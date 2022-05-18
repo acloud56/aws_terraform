e90c31202aa10989d13adbc2bafa31dd8fe88187
acloud56 <35495677+acloud56@users.noreply.github.com>
2022-05-18 10:25:27 -0400

    Update main.tf

--- aws_vpc/main.tf
+++ aws_vpc/main.tf
@@ -1,18 +1,18 @@
-Create the VPC
+
  resource "aws_vpc" "Main" {                # Creating VPC here
    cidr_block       = var.main_vpc_cidr     # Defining the CIDR block use 10.0.0.0/24 for demo
    instance_tenancy = "default"
  }
- Create Internet Gateway and attach it to VPC
+
  resource "aws_internet_gateway" "IGW" {    # Creating Internet Gateway
     vpc_id =  aws_vpc.Main.id               # vpc_id will be generated after we create VPC
  }
- Create a Public Subnets.
+
  resource "aws_subnet" "publicsubnets" {    # Creating Public Subnets
    vpc_id =  aws_vpc.Main.id
    cidr_block = "${var.public_subnets}"        # CIDR block of public subnets
  }
- Create a Private Subnet                   # Creating Private Subnets
+                
  resource "aws_subnet" "privatesubnets" {
    vpc_id =  aws_vpc.Main.id
    cidr_block = "${var.private_subnets}"          # CIDR block of private subnets
