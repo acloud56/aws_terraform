1246d4fe986d86d2efae87714aae2125bdeb7946
acloud56 <35495677+acloud56@users.noreply.github.com>
2022-05-18 10:44:29 -0400

    Update main.tf

--- aws_vpc/main.tf
+++ aws_vpc/main.tf
@@ -10,12 +10,12 @@
 
  resource "aws_subnet" "publicsubnets" {    # Creating Public Subnets
    vpc_id =  aws_vpc.Main.id
-   cidr_block = "${var.public_subnets}"        # CIDR block of public subnets
+   cidr_block = var.public_subnets        # CIDR block of public subnets
  }
                 
  resource "aws_subnet" "privatesubnets" {
    vpc_id =  aws_vpc.Main.id
-   cidr_block = "${var.private_subnets}"          # CIDR block of private subnets
+   cidr_block = var.private_subnets          # CIDR block of private subnets
  }
 
  resource "aws_route_table" "PublicRT" {    # Creating RT for Public Subnet
