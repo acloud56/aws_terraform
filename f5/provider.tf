27942d08be0f4fade8de75a0862eab0f28211917
acloud56 <35495677+acloud56@users.noreply.github.com>
2022-06-07 11:21:04 -0400

    Update provider.tf

--- f5/provider.tf
+++ f5/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     bigip = {
       source = "terraform-providers/bigip"
-      version = "~> 1.6.0"
+      version = "~> 1.10.0"
     }
   }
   required_version = ">= 0.13"
