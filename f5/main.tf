9d2020595bd5222512a8f0cfabc3c2f0ff87e4c3
acloud56 <35495677+acloud56@users.noreply.github.com>
2022-06-07 10:44:02 -0400

    Update main.tf

--- f5/main.tf
+++ f5/main.tf
@@ -42,12 +42,12 @@ resource "bigip_ltm_pool" "pool" {
 }
 resource "bigip_ltm_pool_attachment" "attach_node" {
   pool = bigip_ltm_pool.pool.name
-  node = bigip_ltm_node.node_1.name
+  node = "${bigip_ltm_node.node_1.name}:${var.client_port}"
   depends_on = [bigip_ltm_pool.pool]
 }
 resource "bigip_ltm_pool_attachment" "attach_node2" {
   pool = bigip_ltm_pool.pool.name
-  node = bigip_ltm_node.node_2.name
+  node = "${bigip_ltm_node.node_2.name}:${var.client_port}"
   depends_on = [bigip_ltm_pool.pool]
 }
 resource "bigip_ltm_irule" "http_redirect" {
